9f84cbe8f2d53bea8485051d1031a3a6fc92ebb3
Kartik Watwani <Kartikwatwani3@gmail.com>
2023-07-11 01:29:44 +0530

    Ready for some action?

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,51 +1,88 @@
 from odoo import api,models,fields
+
+from odoo.exceptions import UserError
+
 from dateutil.relativedelta import relativedelta
 
+
 class EstateProperty(models.Model):
+
     _name = 'estate.property'
+
     _description = 'This is an Estate Property object.'
 
+
     name = fields.Char("Title",required=True)
+
     description = fields.Text("Description")
+
     postcode = fields.Char("Postcode")
+
     date_availability = fields.Date("Available From",default=fields.Date.add(fields.Date.today(),months=3),copy=False)
+
     expected_price = fields.Float("Expected Price",required=True)
+
     selling_price = fields.Float("Selling Price",readonly=True,copy=False)
+
     bedrooms = fields.Integer("Bedrooms",default=2)
+
     living_area = fields.Integer("Living Area (sqm)",default=0)
+
     facades = fields.Integer("Facades")
+
     garage = fields.Boolean("Garage")
+
     garden = fields.Boolean("Garden")
+
     garden_area = fields.Integer("Garden Area (sqm)",default=0)
+
     garden_orientation = fields.Selection(string='Garden Orientation',
+
         selection=[('N', 'North'), ('S', 'South'),('E','East'),('W','West')])
+
     active = fields.Boolean(default=True)
-    state = fields.Selection(string='Status',selection=[('New','New'),('Offer Received','Offer Received')
-    ,('Offer Accepted','Offer Accepted'),('Sold','Sold'),('Canceled','Canceled')]
-    ,default='New',required=True,copy=False)
+
+    state = fields.Selection(string='Status',selection=[('new','New'),('offer_received','Offer Received')
+
+    ,('offer_accepted','Offer Accepted'),('sold','Sold'),('canceled','Canceled')]
+
+    ,default='new',required=True,copy=False)
+
     total_area = fields.Integer(
+
         "Total Area (sqm)",
         compute="_compute_total_area",
+
         help="Total area computed by summing the living area and the garden area",
     )
+
     best_price = fields.Integer("Best Offer",compute="_compute_best_offer")
 
+
     #Relational
+
     property_type_id = fields.Many2one('estate.property.type',string="Property Type")
+
     salesperson_id = fields.Many2one('res.users',default=lambda self:self.env.user,string="Salesman")
+
     buyer_id = fields.Many2one('res.partner',copy=False,string="Buyer")
+
     tag_ids = fields.Many2many('estate.property.tag',string="Tags")
+
     offer_ids = fields.One2many('estate.property.offer','property_id',string="Offers")
+
 
     @api.depends('living_area','garden_area')
     def _compute_total_area(self):
         for prop in self:
             prop.total_area = prop.living_area + prop.garden_area  
 
+
     @api.depends("offer_ids")
     def _compute_best_offer(self):
-        for prop in self:
-            prop.best_price = max(prop.offer_ids.mapped('price'))
+        for record in self:
+            record.best_price = max(record.offer_ids.mapped('price'),default=0)
+
 
     @api.onchange("garden")
     def _onchange_garden_state(self):
@@ -54,4 +91,21 @@ class EstateProperty(models.Model):
             self.garden_orientation = 'N'
         else:
             self.garden_area = 0
-            self.garden_orientation = None    
+            self.garden_orientation = None
+
+    def mark_as_sold(self):
+        for record in self:
+            if(record.state == 'canceled'):
+                raise UserError('Canceled property cannot be marked as sold')
+            else: 
+                record.state = 'sold'
+        return True
+
+    def mark_as_canceled(self):
+        for record in self:
+            if(record.state == 'sold'):
+                raise UserError('Sold property cannot be marked as canceled.')
+            else:
+                record.state = 'canceled'    
+
+        return True                             
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,13 +1,13 @@
 from odoo import api, models, fields
 from dateutil.relativedelta import relativedelta
-
+from odoo.exceptions import UserError
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
     _description = "This is offer for an estate property."
 
     price = fields.Float('Price')
-    status = fields.Selection(string='Status', copy=False, selection=[
-                              ('Accepted', 'Accepted'), ('Refused', 'Refused')])
+    state = fields.Selection(string='Status', copy=False, selection=[
+                              ('accepted', 'Accepted'), ('refused', 'Refused')])
 
     validity = fields.Integer('Validity (in days)', default=7)
     date_deadline = fields.Date('Deadline date', compute="_compute_deadline",inverse="_compute_validity")
@@ -24,3 +24,21 @@ class EstatePropertyOffer(models.Model):
         for offer in self:
             date = offer.create_date.date() if offer.create_date else fields.Date.today()
             offer.validity = (offer.date_deadline - date).days
+
+    def action_accept_offer(self):
+     for offer in self:    
+        if('accepted' in self.property_id.offer_ids.mapped('state')):
+            raise UserError('An offer has already been accepted and the property is sold.')
+        else:
+            offer.state = 'accepted'
+            offer.property_id.selling_price = offer.price
+            offer.property_id.buyer_id = offer.partner_id
+            offer.property_id.state = 'sold' 
+
+    def action_refuse_offer(self):
+        for offer in self:
+
+            if(offer.state!='accepted'):
+                offer.state = 'refused'
+            else:
+                raise UserError('This offer has already been accepted.')          
